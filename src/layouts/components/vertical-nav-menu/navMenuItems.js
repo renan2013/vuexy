b22d9410e0199180281a200b12cdbec7db5c6e11
renan2013 <renangalvan@gmail.com>
2020-09-28 17:50:53 -0600

    ok

--- src/layouts/components/vertical-nav-menu/navMenuItems.js
+++ src/layouts/components/vertical-nav-menu/navMenuItems.js
@@ -27,124 +27,124 @@ export default [
   //   icon: "MailIcon",
   //   i18n: "Email",
   // },
-  {
-    url: null,
-    name: 'Dashboard',
-    tag: '2',
-    tagColor: 'warning',
-    icon: 'HomeIcon',
-    i18n: 'Dashboard',
-    submenu: [
-      {
-        url: '/dashboard/analytics',
-        name: 'Analytics',
-        slug: 'dashboard-analytics',
-        i18n: 'Analytics'
-      },
-      {
-        url: '/dashboard/ecommerce',
-        name: 'eCommerce',
-        slug: 'dashboard-ecommerce',
-        i18n: 'eCommerce'
-      }
-    ]
-  },
+  // {
+  //   url: null,
+  //   name: 'Dashboard',
+  //   tag: '2',
+  //   tagColor: 'warning',
+  //   icon: 'HomeIcon',
+  //   i18n: 'Dashboard',
+  //   submenu: [
+  //     {
+  //       url: '/dashboard/analytics',
+  //       name: 'Analytics',
+  //       slug: 'dashboard-analytics',
+  //       i18n: 'Analytics'
+  //     },
+  //     {
+  //       url: '/dashboard/ecommerce',
+  //       name: 'eCommerce',
+  //       slug: 'dashboard-ecommerce',
+  //       i18n: 'eCommerce'
+  //     }
+  //   ]
+  // },
 
-  {
-    header: 'Apps',
-    icon: 'PackageIcon',
-    i18n: 'Apps',
-    items: [
-      {
-        url: '/apps/email',
-        name: 'Email',
-        slug: 'email',
-        icon: 'MailIcon',
-        i18n: 'Email'
-      },
-      {
-        url: '/apps/chat',
-        name: 'Chat',
-        slug: 'chat',
-        icon: 'MessageSquareIcon',
-        i18n: 'Chat'
-      },
-      {
-        url: '/apps/todo',
-        name: 'Todo',
-        slug: 'todo',
-        icon: 'CheckSquareIcon',
-        i18n: 'Todo'
-      },
-      {
-        url: '/apps/calendar/vue-simple-calendar',
-        name: 'Calendar',
-        slug: 'calendar-simple-calendar',
-        icon: 'CalendarIcon',
-        tagColor: 'success',
-        i18n: 'Calendar'
-      },
-      {
-        url: null,
-        name: 'eCommerce',
-        icon: 'ShoppingCartIcon',
-        i18n: 'eCommerce',
-        submenu: [
-          {
-            url: '/apps/eCommerce/shop',
-            name: 'Shop',
-            slug: 'ecommerce-shop',
-            i18n: 'Shop'
-          },
-          {
-            url: '/apps/eCommerce/item/',
-            name: 'Item Details',
-            slug: 'ecommerce-item-detail-view',
-            i18n: 'ItemDetails'
-          },
-          {
-            url: '/apps/eCommerce/wish-list',
-            name: 'Wish List',
-            slug: 'ecommerce-wish-list',
-            i18n: 'WishList'
-          },
-          {
-            url: '/apps/eCommerce/checkout',
-            name: 'Checkout',
-            slug: 'ecommerce-checkout',
-            i18n: 'Checkout'
-          }
-        ]
-      },
-      {
-        url: null,
-        name: 'User',
-        icon: 'UserIcon',
-        i18n: 'User',
-        submenu: [
-          {
-            url: '/apps/user/user-list',
-            name: 'List',
-            slug: 'app-user-list',
-            i18n: 'List'
-          },
-          {
-            url: '/apps/user/user-view/268',
-            name: 'View',
-            slug: 'app-user-view',
-            i18n: 'View'
-          },
-          {
-            url: '/apps/user/user-edit/268',
-            name: 'Edit',
-            slug: 'app-user-edit',
-            i18n: 'Edit'
-          }
-        ]
-      }
-    ]
-  },
-  {
+  // {
+  //   header: 'Apps',
+  //   icon: 'PackageIcon',
+  //   i18n: 'Apps',
+  //   items: [
+  //     {
+  //       url: '/apps/email',
+  //       name: 'Email',
+  //       slug: 'email',
+  //       icon: 'MailIcon',
+  //       i18n: 'Email'
+  //     },
+  //     {
+  //       url: '/apps/chat',
+  //       name: 'Chat',
+  //       slug: 'chat',
+  //       icon: 'MessageSquareIcon',
+  //       i18n: 'Chat'
+  //     },
+  //     {
+  //       url: '/apps/todo',
+  //       name: 'Todo',
+  //       slug: 'todo',
+  //       icon: 'CheckSquareIcon',
+  //       i18n: 'Todo'
+  //     },
+  //     {
+  //       url: '/apps/calendar/vue-simple-calendar',
+  //       name: 'Calendar',
+  //       slug: 'calendar-simple-calendar',
+  //       icon: 'CalendarIcon',
+  //       tagColor: 'success',
+  //       i18n: 'Calendar'
+  //     },
+  //     {
+  //       url: null,
+  //       name: 'eCommerce',
+  //       icon: 'ShoppingCartIcon',
+  //       i18n: 'eCommerce',
+  //       submenu: [
+  //         {
+  //           url: '/apps/eCommerce/shop',
+  //           name: 'Shop',
+  //           slug: 'ecommerce-shop',
+  //           i18n: 'Shop'
+  //         },
+  //         {
+  //           url: '/apps/eCommerce/item/',
+  //           name: 'Item Details',
+  //           slug: 'ecommerce-item-detail-view',
+  //           i18n: 'ItemDetails'
+  //         },
+  //         {
+  //           url: '/apps/eCommerce/wish-list',
+  //           name: 'Wish List',
+  //           slug: 'ecommerce-wish-list',
+  //           i18n: 'WishList'
+  //         },
+  //         {
+  //           url: '/apps/eCommerce/checkout',
+  //           name: 'Checkout',
+  //           slug: 'ecommerce-checkout',
+  //           i18n: 'Checkout'
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       url: null,
+  //       name: 'User',
+  //       icon: 'UserIcon',
+  //       i18n: 'User',
+  //       submenu: [
+  //         {
+  //           url: '/apps/user/user-list',
+  //           name: 'List',
+  //           slug: 'app-user-list',
+  //           i18n: 'List'
+  //         },
+  //         {
+  //           url: '/apps/user/user-view/268',
+  //           name: 'View',
+  //           slug: 'app-user-view',
+  //           i18n: 'View'
+  //         },
+  //         {
+  //           url: '/apps/user/user-edit/268',
+  //           name: 'Edit',
+  //           slug: 'app-user-edit',
+  //           i18n: 'Edit'
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // },
+ /*  {
     header: 'UI',
     icon: 'LayersIcon',
     i18n: 'UI',
@@ -502,8 +502,8 @@ export default [
         ]
       }
     ]
-  },
-  {
+  }, */
+ /*  {
     header: 'Forms & Table',
     icon: 'Edit3Icon',
     i18n: 'FormsAndTable',
@@ -601,8 +601,8 @@ export default [
         i18n: 'agGridTable'
       }
     ]
-  },
-  {
+  }, */
+ /*  {
     header: 'Pages',
     icon: 'FileIcon',
     i18n: 'Pages',
@@ -739,8 +739,8 @@ export default [
         ]
       }
     ]
-  },
-  {
+  }, */
+ /*  {
     header: 'Charts & Maps',
     icon: 'PieChartIcon',
     i18n: 'ChartsAndMaps',
@@ -781,9 +781,9 @@ export default [
         i18n: 'GoogleMap'
       }
     ]
-  },
+  }, */
 
-  {
+ /*  {
     url: null,
     name: 'Menu',
     tag: '2',
@@ -822,6 +822,30 @@ export default [
 
       }
     ]
+  }, */
+  {
+        url: '/',
+        name: 'Inicio',
+        slug: 'dashboard-analytics',
+        icon: 'HomeIcon',  
+  },
+  {
+    url: '/pages/profile',
+    name: 'Quienes somos',
+    slug: 'dashboard-analytics',
+    icon: 'HomeIcon',  
+  },
+  {
+    url: '/apps/eCommerce/wish-list',
+    name: 'Productos',
+    slug: 'dashboard-analytics',
+    icon: 'HomeIcon',  
+  },
+  {
+    url: '/apps/eCommerce/wish-list',
+    name: 'Servicios',
+    slug: 'dashboard-analytics',
+    icon: 'HomeIcon',  
   },
 ]
 
