08450e20e00c186d2f9913b1ce29ccef8b439bc4
renan2013 <renangalvan@gmail.com>
2020-09-28 17:13:09 -0600

    ok

--- src/layouts/components/TheFooter.vue
+++ src/layouts/components/TheFooter.vue
@@ -1,14 +1,3 @@
-<!-- =========================================================================================
-    File Name: TheFooter.vue
-    Description: Footer component
-    Component Name: TheFooter
-    ----------------------------------------------------------------------------------------
-    Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
-      Author: Pixinvent
-    Author URL: http://www.themeforest.net/user/pixinvent
-========================================================================================== -->
-
-
 <template functional>
     <footer class="the-footer flex-wrap justify-between" :class="classes">
         <p>
